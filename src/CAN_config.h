dd54461b10075f0b332c8e7a92a05309e1b01bb9
Sjoerd Cranen <scranen@gmail.com>
2012-01-19 17:05:08 +0100

    Moved some configuration to CAN_config.h. Eventually, the MSCAN base register should also go there. Also implemented critical section for freeing a buffer using a muCOS/II semaphore.

---
+++ src/CAN_config.h
@@ -0,0 +1,17 @@
+/*
+ * CAN_config.h
+ *
+ *  Created on: Jan 19, 2012
+ *      Author: S. Cranen
+ */
+
+#ifndef CAN_CONFIG_H_
+#define CAN_CONFIG_H_
+
+#define CAN_MUTEX_PRIO        OS_LOWEST_PRIO
+#define CAN_CLOCK_OSC_SPEED         4000000u //  4Mhz
+#define CAN_CLOCK_BUS_SPEED        20000000u // 20Mhz
+#define CAN_MSG_BUFFER_SIZE              10u
+#define CAN_MAX_QUEUES                   10u
+
+#endif /* CAN_CONFIG_H_ */
